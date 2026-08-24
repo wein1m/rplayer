e51ab598b1372c41888aefdb9d72c94ce5465c48
wein1m <weynn.sz@gmail.com>
2026-08-25 01:17:00 +0800

    feat: fetch music metadata

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,9 +1,23 @@
 use std::{fs, path::Path};
 
+use lofty::{file::AudioFile, read_from_path};
+use lofty::file::TaggedFileExt;
+use lofty::tag::Accessor;
+
 use walkdir::{WalkDir, DirEntry};
 
-pub fn scan_music() -> Result<(), Box<dyn std::error::Error>> {
-    let path = "/home/wein/rplayer-test/";
+#[derive(Debug)]
+pub struct TrackAudio {
+    pub path: String,
+    pub track_title: String,
+    pub track_artist: String,
+    pub track_duration: f32,
+}
+
+pub fn scan_music() -> Result<Vec<TrackAudio>, Box<dyn std::error::Error>> {
+    let path = "assets";
+
+    let mut tracks = Vec::new();
 
     let walker = WalkDir::new(path).into_iter();
     for entry in walker.filter_entry(|e| !is_hidden(e)) {
@@ -12,14 +26,16 @@ pub fn scan_music() -> Result<(), Box<dyn std::error::Error>> {
                 let path = e.path();
 
                 if path.is_file() && is_music(path) {
-                    println!("{}", e.path().display())
+                    if let Some(track) = create_track(path) {
+                        tracks.push(track);
+                    }
                 };
             },
             Err(e) => eprintln!("Error: {}", e),
         }
     }
-        
-    Ok(())
+
+    Ok(tracks)
 }
 
 fn is_hidden(entry: &DirEntry) -> bool {
@@ -36,4 +52,44 @@ fn is_music(path: &Path) -> bool {
     } else {
         false
     }
+}
+
+fn create_track(path: &Path) -> Option<TrackAudio> {
+    let abs_path = fs::canonicalize(path).ok()?;
+    let mut track_title = String::new();
+    let mut track_artist = String::new();
+
+    let tagged_file = match read_from_path(path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Failed to read {}: {}", path.display(), e);
+            return None
+        }
+    };
+
+    if let Some(tag) = tagged_file.primary_tag() {
+        if let Some(title) = tag.title() {
+            track_title = title.to_string();
+            
+            if track_title.is_empty() {
+                track_title = path.file_stem().unwrap().to_string_lossy().into();
+            }
+        }
+        if let Some(artist) = tag.artist() {
+            track_artist = artist.to_string();
+
+            if track_artist.is_empty() {
+                track_artist = String::from("Unknown Artist");
+            }
+        }
+    }
+
+    let track_duration = tagged_file.properties().duration().as_secs_f32();
+
+    Some(TrackAudio {
+        path: abs_path.to_string_lossy().to_string(),
+        track_title,
+        track_artist,
+        track_duration
+    })
 }
